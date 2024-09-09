917ac7d791a17fd849f87a5f8bb4e08342dd4480
Mac <mac@Macs-MacBook-Pro.local>
2024-09-09 23:33:28 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ScrollView, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const App = () => {
+  return (
+    <ScrollView>
+      <View style={styles}></View>
+    
+    
+    </ScrollView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
